03fbe91edf982444ec5af5c0a3623120433bd89e
Plouzennec Benjamin <plouzennec.benjamin@gmail.com>
2018-03-07 21:05:24 +0100

    :tada::bug:

---
+++ bootstrap-vue-nuxt/components/MyButton.vue
@@ -0,0 +1,14 @@
+<template>
+  <b-button>Hey</b-button>
+</template>
+<script>
+import bButton from 'bootstrap-vue/es/components/button/button';
+
+export default {
+  components: {
+    bButton,
+  }
+}
+</script>
+<style>
+</style>
---
+++ bootstrap-vue-nuxt/pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <MyButton />
+</template>
+
+<script>
+import MyButton from '~/components/MyButton.vue'
+
+export default {
+  components: {
+    MyButton,
+  }
+}
+</script>
+
+<style>
+</style>
